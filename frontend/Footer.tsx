34ef7d79024818ef1a45ce9dea87913fe40cf52d
melpalhano <mlssplhn@gmail.com>
2024-09-01 11:36:23 -0300

    feat: add a landing page structure to the estacao tech

---
+++ frontend/Footer.tsx
@@ -0,0 +1,33 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="w-full pt-20 pb-10" id="contact">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50 "
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Frase e Efeito <span className="text-blue">para</span> Projetos
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Lorem Ipsu Dolor Ticalacata
+        </p>
+      </div>  
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Estação Tech
+        </p>
+
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/data/index.ts
@@ -0,0 +1,39 @@
+export const navItems = [
+  { name: "Sobre", link: "" },
+  { name: "Projetos", link: "" },
+  { name: "Gestores", link: "" },
+  { name: "Embaixadores", link: "" },
+];
+
+
+export const workProjetos = [
+  {
+    id: 1,
+    title: "Projeto 01",
+    desc: "Descrevendo o Projeto",
+    className: "md:col-span-2",
+    thumbnail: "/img.png",
+  },
+  {
+    id: 2,
+    title: "Projeto 02",
+    desc: "Descrevendo o Projeto",
+    className: "md:col-span-2",
+    thumbnail: "/img.png",
+  },
+  {
+    id: 3,
+    title: "Projeto 03",
+    desc: "Descrevendo o Projeto",
+    className: "md:col-span-2",
+    thumbnail: "/img.png",
+  },
+  {
+    id: 4,
+    title: "Projeto 04",
+    desc: "Descrevendo o Projeto",
+    className: "md:col-span-2",
+    thumbnail: "/img.png",
+  },
+];
+
