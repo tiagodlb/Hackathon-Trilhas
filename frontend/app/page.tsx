324ac611e15e7be865fd5dd548d80adbbc41ff8e
melpalhano <mlssplhn@gmail.com>
2024-09-01 11:36:23 -0300

    feat: add a landing page structure to the estacao tech

---
+++ frontend/app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Projetos from "@/components/Projetos";
+import Sobre from "@/components/Sobre";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
+
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Sobre />
+        <Projetos />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ frontend/components/ui/RepresentantesCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const RepresentanteCard: React.FC = () => {
+  return (
+    <main className="pt-32">
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '40px' }}>
+        <div style={{ flex: 1, paddingRight: '20px' }}>
+          {/* <h1 className="text-2xl">
+          Nossa <span className="text-[#81B7FF]">História</span>
+          </h1> */}
+          <p>
+           Lorel Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Donec Eget Nunc Vel Nunc Malesuada Tincidunt. Nulla Facilisi. Nulla Tempor, 
+          </p>
+        </div>
+        <div style={{ flexShrink: 0 }}>
+          <img src="./img.png" alt="Sobre nós" style={{ maxWidth: '550px', height: '360px' }} />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default RepresentanteCard;
